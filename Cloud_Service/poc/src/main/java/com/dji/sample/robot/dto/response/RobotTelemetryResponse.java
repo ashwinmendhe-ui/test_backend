ca9761470530981aaefbfadbb1784f3dc7ed5689
Ashwin Mendhe <ashwin.mendhe@dhive.pro>
2026-08-07 16:42:06 +0530

    add robot telemetry all fields

--- Cloud_Service/poc/src/main/java/com/dji/sample/robot/dto/response/RobotTelemetryResponse.java
+++ Cloud_Service/poc/src/main/java/com/dji/sample/robot/dto/response/RobotTelemetryResponse.java
@@ -1,14 +1,40 @@
 package com.dji.sample.robot.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RobotTelemetryResponse {
-    private Integer battery;
+
+    private Double battery;
+
+    private String status;
+
+    private String network;
+
+    private String gps;
+    private String gpsFix;
+
     private Double speed;
+
     private Double latitude;
     private Double longitude;
+
+    private Double altitude;
     private Double heading;
-    private String network;
+
+    private Integer rssi;
+    private Integer rsrp;
+    private Integer latencyMs;
+
+    private Double voltage;
+    private Boolean charging;
+
+    private String deviceSn;
+    private String sourceDeviceSn;
+    private String gateway;
+    private String deviceType;
+
     private String timestamp;
 }
